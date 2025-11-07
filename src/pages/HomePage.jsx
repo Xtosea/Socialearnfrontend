2c775221461fd915dc000e3ead498c3dc9c7eafc
Chris <106016563+Xtosea@users.noreply.github.com>
2025-11-07 09:45:50 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -22,7 +22,6 @@ import HeroBanner from "../components/HeroBanner";
 import GrowSection from "../components/GrowSection";
 import Footer from "../components/Footer";
 import MonetagBanner from "../components/ads/MonetagBanner";
-//import MonetagSmartTag from "../components/ads/MonetagSmartTag";
 
 export default function HomePage() {
   const navigate = useNavigate();
