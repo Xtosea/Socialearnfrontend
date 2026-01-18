3cbfa2bfad5adacc157d3c3e42fb0c62323463d9
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-18 18:14:11 +0100

    Update SocialEmbed.jsx

--- src/components/SocialEmbed.jsx
+++ src/components/SocialEmbed.jsx
@@ -1,50 +1,36 @@
 import React from "react";
-import {
-  FacebookEmbed,
-  YouTubeEmbed,
-  TikTokEmbed,
-  InstagramEmbed,
-} from "react-social-media-embed";
 
-const SocialEmbed = ({ url, width = 350 }) => {
+export default function SocialEmbed({ url }) {
   if (!url) return null;
 
-  if (url.includes("youtube.com") || url.includes("youtu.be")) {
-    return <YouTubeEmbed url={url} width={width} height={250} />;
-  } 
-  else if (url.includes("facebook.com")) {
-    return (
-      <div>
-        <FacebookEmbed url={url} width={width} />
-        <a href={url} target="_blank" rel="noreferrer">
-          Watch on Facebook
-        </a>
-      </div>
-    );
-  } 
-  else if (url.includes("tiktok.com")) {
-    return (
-      <div>
-        <TikTokEmbed url={url} width={width} />
-        <a href={url} target="_blank" rel="noreferrer">
-          Watch on TikTok
-        </a>
-      </div>
-    );
-  } 
-  else if (url.includes("instagram.com")) {
-    return (
-      <div>
-        <InstagramEmbed url={url} width={width} captioned />
-        <a href={url} target="_blank" rel="noreferrer">
-          Watch on Instagram
-        </a>
-      </div>
-    );
-  } 
-  else {
-    return <p>Unsupported platform</p>;
-  }
-};
+  let embedUrl = url;
 
-export default SocialEmbed;
+  if (url.includes("youtube.com") || url.includes("youtu.be")) {
+    const id = url.includes("youtu.be") ? url.split("/").pop() : new URL(url).searchParams.get("v");
+    embedUrl = `https://www.youtube.com/embed/${id}?modestbranding=1&rel=0`;
+  } else if (url.includes("tiktok.com")) {
+    embedUrl = url.replace("/video/", "/embed/v2/");
+  } else if (url.includes("facebook.com") || url.includes("fb.watch")) {
+    embedUrl = `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(url)}&show_text=false`;
+  } else if (url.includes("instagram.com")) {
+    embedUrl = `${url}embed`;
+  } else if (url.includes("twitter.com")) {
+    embedUrl = `https://twitframe.com/show?url=${encodeURIComponent(url)}`;
+  } else if (url.includes("linkedin.com")) {
+    embedUrl = url.replace("/posts/", "/embed/feed/");
+  } else {
+    return <p>Unsupported URL</p>;
+  }
+
+  return (
+    <div className="relative w-full pb-[56.25%] h-0 overflow-hidden rounded-lg">
+      <iframe
+        src={embedUrl}
+        className="absolute top-0 left-0 w-full h-full"
+        frameBorder="0"
+        allowFullScreen
+        title="Social Embed"
+      />
+    </div>
+  );
+}
